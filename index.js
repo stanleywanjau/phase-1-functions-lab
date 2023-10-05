057a2a8975ab90f3ef86fcd2632c2609b0d0aed6
stanleywanjau <stanleywanjau371@gmail.com>
2023-10-05 11:38:59 -0700

    intial commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+let distanceFromHqInBlocks=function (somevalue){
+  if (somevalue>42){
+    return somevalue-42
+  }else 
+  return Math.abs(somevalue- 42);  
+}
+
+function distanceFromHqInFeet(somevalue){
+  console.log(distanceFromHqInBlocks(somevalue));
+  return distanceFromHqInBlocks(somevalue)*264;
+}
+
+/*function distanceTravelledInFeet(somevalue){
+  somevalue
+}
+distanceTravelledInFeet(d)*/
+let Feet=distanceFromHqInBlocks()*264;
+function distanceTravelledInFeet(number){
+  return Feet;
+}
