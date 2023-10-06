e92c6a96b0be941add3df44b3051335c4e820970
stanleywanjau <stanleywanjau371@gmail.com>
2023-10-06 11:43:15 -0700

    completed assignment

--- index.js
+++ index.js
@@ -11,11 +11,33 @@ function distanceFromHqInFeet(somevalue){
   return distanceFromHqInBlocks(somevalue)*264;
 }
 
-/*function distanceTravelledInFeet(somevalue){
-  somevalue
+function distanceTravelledInFeet(start,destination){
+  if (destination>42){
+    let feet=((destination-start)*264)
+    return feet
+  } else {
+    let feet =((start-destination)*264)
+    return Math.abs(feet)
+  }
 }
-distanceTravelledInFeet(d)*/
-let Feet=distanceFromHqInBlocks()*264;
-function distanceTravelledInFeet(number){
-  return Feet;
+
+function calculatesFarePrice(start,destination){
+  let distance=distanceTravelledInFeet(start,destination)
+  let fare=0
+  if (distance <= 400){
+    return fare
+  }
+  else if (distance>400 && distance<=2000){
+    distance=distance - 400;
+    fare= distance * 0.02
+    return fare
+  }
+  else if (distance >2000 && distance <=2499){
+    let fare=25
+    return fare
+  }
+  else if  (distance>=2500){
+    return "cannot travel that far"
+  }
 }
+console.log(calculatesFarePrice(4,11))
